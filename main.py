46ca133a539139c14e7f4209b28048b50c1b2595
dantar14 <111466337+dantar14@users.noreply.github.com>
2022-08-27 17:48:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import sumar as s
+import restar as r
+import multiplicar as m
+import division as d
+import cuadrado as c
+
+if __name__=="__main__":
+    a=10
+    b=5
+    print(s.sumar(a,b))
+    print(r.restar(a,b))
+    print(m.multiplicar(a,b))
+    print(d.division(a,b))
+    print(c.cuadrado(a))
+    print(c.cuadrado(b))
---
+++ operaciones.py
@@ -0,0 +1,15 @@
+def sumar(a:int ,b:int)->int:
+    return a+b
+
+def restar(a:int ,b:int)->int:
+    return a-b
+
+def multiplicar(a:int,b:int)->int:
+    return a*b
+
+def division(a:float,b:float)->float:
+    return a/b
+
+def cuadrado(a:int)->int:
+    return a*a
+         
